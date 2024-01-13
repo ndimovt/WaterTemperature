1746d8e2d589813e6da97caa62c01562c2f1af5e
ndimovt <nikolaitodorov119@gmail.com>
2024-01-13 12:38:28 +0200

    -updated all try blocks with try-with-resources in DBReading.java and DBWriting.java -removed close() methods

--- src/main/java/mysqlutilitypackage/DBReading.java
+++ src/main/java/mysqlutilitypackage/DBReading.java
@@ -3,17 +3,16 @@ package mysqlutilitypackage;
 import io.github.ndimovt.Scientist;
 import io.github.ndimovt.WaterInformation;
 
-import java.sql.*;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DBReading {
     private static WaterInformation waterInfo;
     private static Scientist scientist;
     public static Scientist getAccountInformation() {
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try{
-            preparedStatement = DBConnection.getConnection().prepareStatement("call water_temperature_information.getFullData()");
-            resultSet = preparedStatement.executeQuery();
+        try(PreparedStatement preparedStatement = DBConnection.getConnection().prepareStatement("call water_temperature_information.getFullData()");
+        ResultSet resultSet = preparedStatement.executeQuery()){
             while (resultSet.next()){
                 scientist = new Scientist(
                         resultSet.getString("username"),
@@ -23,19 +22,14 @@ public class DBReading {
         }catch (SQLException sqe){
             System.out.println("No connection to database! Please try again later or call your IT support!");
             sqe.printStackTrace();
-        } finally {
-            closeConnection(preparedStatement,resultSet,DBConnection.getConnection());
         }
         return scientist;
     }
 
     public static void getWaterInformation(String city) {
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try{
-            preparedStatement = DBConnection.getConnection().prepareStatement("call water_temperature_information.getTownReadings(?)");
+        try(PreparedStatement preparedStatement = DBConnection.getConnection().prepareStatement("call water_temperature_information.getTownReadings(?)");
+        ResultSet resultSet = preparedStatement.executeQuery()){
             preparedStatement.setString(1,city);
-            resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
                  waterInfo = new WaterInformation(
                         resultSet.getString("m_date"),
@@ -47,24 +41,5 @@ public class DBReading {
             System.out.println("No connection to database! Please try again later or call your IT support!");
             sqe.printStackTrace();
         }
-        finally {
-            closeConnection(preparedStatement,resultSet,DBConnection.getConnection());
-        }
     }
-    private static void closeConnection(PreparedStatement preparedStatement, ResultSet resultSet, Connection connection){
-        try{
-            if(preparedStatement != null){
-                preparedStatement.close();
-            }
-            if(resultSet != null){
-                resultSet.close();
-            }
-            if(connection != null){
-                connection.close();
-            }
-        }catch (SQLException sqe){
-            sqe.printStackTrace();
-        }
-    }
-
 }
--- src/main/java/mysqlutilitypackage/DBWriting.java
+++ src/main/java/mysqlutilitypackage/DBWriting.java
@@ -1,6 +1,5 @@
 package mysqlutilitypackage;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -8,9 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class DBWriting {
     public static void addCityAndTemperatureToDB(String city, double temperature) {
-        PreparedStatement pst = null;
-        try {
-            pst = DBConnection.getConnection().prepareStatement("call water_temperature_information.insertData(?,?,?)");
+        try (PreparedStatement pst = DBConnection.getConnection().prepareStatement("call water_temperature_informatio.insertData(?,?,?)")){
             pst.setString(1, city);
             pst.setString(2, date());
             pst.setDouble(3, temperature);
@@ -18,27 +15,11 @@ public class DBWriting {
         } catch (SQLException sqlException) {
             System.out.println("No connection to database! Please try again later or call your IT support!");
             sqlException.printStackTrace();
-        } finally {
-            closeConnection(pst, DBConnection.getConnection());
         }
     }
-
     private static String date() {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDate = LocalDate.now();
         return localDate.format(format);
-    }
-
-    private static void closeConnection(PreparedStatement preparedStatement, Connection connection) {
-        try {
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException sqe) {
-            sqe.printStackTrace();
-        }
     }
 }
